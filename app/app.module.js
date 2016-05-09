347787740d54861e1eaf3ee250437c97e28e2467
Lawrence23 <lawrence@qburst.com>
2016-05-09 14:25:22 +0530

    code restructure

---
+++ app/app.module.js
@@ -0,0 +1,5 @@
+(function() {
+	'use strict';
+
+  	window.app = angular.module('plannerApp', ['ui.router','ngStorage','ui.sortable']);
+})();
--- app/scripts/app.module.js
+++
@@ -1,1 +0,0 @@
-var app = angular.module('plannerApp', ['ui.router','ngStorage','ui.sortable']);
